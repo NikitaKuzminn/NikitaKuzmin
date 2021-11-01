8954acc360fd698110cd5490a19df098c1641f92
Nikkuzminn <oklertoto@gmail.com>
2021-11-01 18:52:36 +0300

    requested_changes_update

--- src/test/java/home/epam/hw3/pages/LeftMenu.java
+++ src/test/java/home/epam/hw3/pages/LeftMenu.java
@@ -3,11 +3,13 @@ package home.epam.hw3.pages;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+@EqualsAndHashCode(callSuper = false)
 @Data
 public class LeftMenu extends BasePage {
 
